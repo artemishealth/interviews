d23cff2c20ccecb14852474cceda4ae5dec11b60
Zach Bakker <zach.bakker@nomihealth.com>
2024-04-03 09:02:31 -0500

    add algo

---
+++ backend/algo.py
@@ -0,0 +1,13 @@
+"""
+Implement the function `random_walk` that simulates a 2-dimensional random walk.
+In a 2-dimensional walk, at each step, the position should move either up, down, left, or right with equal probability.
+After each step if the walker has returned home (where the walk started) print `"home"` and stop walking.
+If the walker doesn't return home after the max steps of the walk print `"lost"`.
+"""
+
+
+def random_walk(max_steps: int = 100) -> None: ...
+
+
+if __name__ == "__main__":
+    random_walk()
